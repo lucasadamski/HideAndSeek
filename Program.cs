c863f222415ac6ff81a89a9ee82e65c1363e7180
lucassodreamer <lukaszadamczy@gmail.com>
2023-03-22 11:39:28 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HideAndSeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            GameController gameController = new GameController();
+            while (true)
+            {
+                Console.WriteLine(gameController.Status);
+                Console.Write(gameController.Prompt);
+                Console.WriteLine(gameController.ParseInput(Console.ReadLine()));
+            }
+            */
+        }
+    }
+}
